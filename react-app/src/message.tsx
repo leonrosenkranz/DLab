bc98529ab254a6d23d678bd71914f26dc5b7d95f
Leon Rosenkranz <Leon.rosenkranz@tu-dortmund.de>
2023-04-28 22:03:04 +0200

    1

---
+++ react-app/src/message.tsx
@@ -0,0 +1,11 @@
+// PascalCasing
+function Message() {
+    // JSX: JavaScript XML
+    const name = 'SUSTAIN Challenge';
+    if (name)
+        return <h1>Hello {name}</h1>
+    return <h1>Hello world</h1>
+}
+
+export default Message;
+
